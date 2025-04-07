cda2331e21cf2dc5a0ff5da6a4196e2be96f1bab
Doris Wei <dwdoriswei@gmail.com>
2025-04-07 11:14:38 -0700

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -57,8 +57,8 @@ function App() {
     setDownloadedFiles([]);
     
     try {
-      // const response = await fetch("/api/scrape", {
-      const response = await fetch("http://127.0.0.1:8000/api/scrape", {
+      const response = await fetch("/api/scrape", {
+      // const response = await fetch("http://127.0.0.1:8000/api/scrape", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

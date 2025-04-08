28e3a5cc72dd1c3470d4eefe7093d90ba60dd802
Doris Wei <dwdoriswei@gmail.com>
2025-04-08 09:32:44 -0700

    fix paths?

--- api/server.py
+++ api/server.py
@@ -34,7 +34,7 @@ def get_s3_client():
 
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="/app/dist", html=True), name="static")
+app.mount("/", StaticFiles(directory="/market-research-app/dist", html=True), name="static")
 
 # CORS Configuration
 app.add_middleware(
